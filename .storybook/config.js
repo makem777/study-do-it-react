d60e9c75a16c941758be63d918586a896b2c2c0a
makem777 <makem777@gmail.com>
2020-07-27 22:58:48 +0900

    04-1 비주얼 테스트로 더 쉽게 개발하기

---
+++ .storybook/config.js
@@ -0,0 +1,14 @@
+import { configure } from '@storybook/react';
+import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
+import JSXAddon from 'storybook-addon-jsx';
+
+function loadStories() {
+  const context = require.context('../src/stories', true, /Story\.jsx$/);
+  context.keys().forEach(srcFile => {
+    // context() 함수로 임포트한 파일 중 default 항목을 임포트합니다.
+    interopRequireDefault(context(srcFile));
+  });
+}
+
+setAddon(JSXAddon);
+configure(loadStories, module);
