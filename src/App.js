f1d64c864ba714582a955c3a1c305cc369f6c921
makem777 <makem777@gmail.com>
2020-07-28 23:26:39 +0900

    04-2 CSS로 컴포넌트 스타일 적용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './sass/materialize.scss';
 import './App.css';
-import PropsComponent from './03/PropsComponent';
 
 class App extends Component {
   render() {
-    return <PropsComponent name="두잇 리액트" />;
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <div>두잇!리액트 시작하기</div>
+          </div>
+        </nav>
+        <h1>머터리얼 CSS</h1>
+      </div>
+    );
   }
 }
 
