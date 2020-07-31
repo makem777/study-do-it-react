1df7bc71bacb11ad6647bda36915eb17a1452095
makem777 <makem777@gmail.com>
2020-07-31 16:32:36 +0900

    04-4 테스트 위주 개발 방법 사용해보기

---
+++ src/__tests__/04/Input_spec.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Input from '../../03/Input';
+
+describe('<Input>', () => {
+  it('renders without crashing', () => {
+    expect(() => {
+      shallow(<Input name="test_name" />);
+    }).not.toThrow();
+  });
+  it('has one element', () => {
+    const wrapper = shallow(<Input name="test_name" />);
+    expect(wrapper.find('input')).toHaveLength(1);
+    expect(wrapper.find('label')).toHaveLength(1);
+  });
+  it('assign the prop value and type', () => {
+    const expectedValue = '123';
+    const wrapper = shallow(<Input name="test_name" value={expectedValue} />);
+    expect(wrapper.find('input').prop('value')).toBe(expectedValue);
+    const { type, value } = wrapper.find('input').props();
+    expect(value).toBe(expectedValue);
+    expect(type).toBe('text');
+  });
+  it('renders errorMessage', () => {
+    const wrapper = shallow(<Input name="test_name" />);
+    expect(wrapper.find('.error')).toHaveLength(0);
+    const expectedErrorMessage = '옳지 못한 값이 입력되었습니다.';
+    wrapper.setProps({ errorMessage: expectedErrorMessage });
+    expect(wrapper.find('span').prop('className')).toBe('error');
+    expect(wrapper.find('.error')).toHaveLength(1);
+    expect(wrapper.html()).toContain(expectedErrorMessage);
+  });
+  it('calls back onChange on input change', () => {
+    const changeSub = jest.fn();
+    const wrapper = shallow(<Input name="test_name" onChange={changeSub} />);
+    expect(changeSub).not.toHaveBeenCalled(); // 호출되지 않은 상태 검증
+    const expectedTargetValue = 'update input';
+    wrapper.find('input').simulate('change', { target: { value: expectedTargetValue } });
+    expect(changeSub).toHaveBeenCalledTimes(1);
+    expect(changeSub).toHaveBeenCalledWith('test_name', expectedTargetValue);
+  });
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,19 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.3';
+import * as Aphrodite from 'aphrodite';
+import * as AphroditeNoImportant from 'aphrodite/no-important';
+
+Aphrodite.StyleSheetTestUtils.suppressStyleInjection(); // 아프로디테의 DOM 함수 호출 과정을 중지시킨다.
+AphroditeNoImportant.StyleSheetTestUtils.suppressStyleInjection(); // 아프로디테의 DOM 함수 호출 과정을 중지시킨다.
+
+configure({ adapter: new Adapter() });
+
+afterEach(() => {
+  console.error.mockClear();
+});
+
+beforeEach(() => {
+  jest.spyOn(console, 'error').mockImplementation(e => {
+    throw new Error(e);
+  });
+});
