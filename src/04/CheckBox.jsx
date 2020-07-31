6f6739f2a39d04292d139790f21595a8cf40a615
makem777 <makem777@gmail.com>
2020-07-31 20:48:38 +0900

    04-5 CheckBox 컴포넌트 만들면서 복습하기

---
+++ src/04/CheckBox.jsx
@@ -0,0 +1,64 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import withStyles, { css } from './withStyles';
+
+class CheckBox extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.setRef = this.setRef.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    if (this.props.autoFocus) {
+      this.ref.focus();
+    }
+  }
+  handleClick(e) {
+    const { name, onChange } = this.props;
+    onChange(name, e.target.checked);
+  }
+  setRef(ref) {
+    this.ref = ref;
+  }
+  render() {
+    const { errorMessage, label, children, styles, checked } = this.props;
+    return (
+      <label>
+        {label}
+        <div>
+          <input
+            ref={this.setRef}
+            type="checkbox"
+            checked={checked && 'checked'}
+            onClick={this.handleClick}
+          />
+          {children}
+        </div>
+        {errorMessage && (
+          <div>
+            <span {...css(styles.errorMessage)}>{errorMessage}</span>
+          </div>
+        )}
+      </label>
+    );
+  }
+}
+
+CheckBox.propTypes = {
+  name: PropTypes.string.isRequired,
+  autoFocus: PropTypes.bool,
+  checked: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+CheckBox.defaultProps = {
+  autoFocus: false,
+  checked: false,
+  onClick: () => {},
+};
+
+export default withStyles(({ color, size }) => ({
+  errorText: {
+    fontSize: size.nm,
+    color: color.error,
+  },
+}))(CheckBox);
