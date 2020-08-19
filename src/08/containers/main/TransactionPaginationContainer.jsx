886b31540357bb0e37fc2b43d9852fb00d29beeb
makem777 <makem777@gmail.com>
2020-08-19 17:01:46 +0900

    11-3 셀렉터로 스토어 데이터 변환하기

--- src/08/containers/main/TransactionPaginationContainer.jsx
+++ src/08/containers/main/TransactionPaginationContainer.jsx
@@ -4,16 +4,16 @@ import {
   requestTransactionList,
   FETCH_TRANSACTION_LIST,
 } from '../../actions/transactionPackActions';
+import { loadingStateSelector } from '../../selectors/transactionSelectors';
 
 const mapStateToProps = state => {
-  const { pagination, loadingState, ids } = state.transactions;
-  const loading = loadingState[FETCH_TRANSACTION_LIST];
+  const { pagination, ids } = state.transactions;
   const { number, size } = pagination;
   return {
     searchParams: state.searchFilter.params,
     pageNumber: number || 1,
     hasNext: ids.length === size,
-    loading,
+    loading: loadingStateSelector(state),
   };
 };
 
